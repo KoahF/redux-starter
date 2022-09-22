42ef202765481c872559b5577174638ff15a1579
KoahF <khoakg2013@gmail.com>
2022-09-22 16:32:19 +0700

    devtools

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import store from './store';
 import { bugAdded, bugRemoved, bugResolved } from './actions';
 
 store.dispatch(bugAdded('Bug 1'));
+store.dispatch(bugAdded('Bug 2'));
+store.dispatch(bugAdded('Bug 3'));
 console.log(store.getState());
 
 store.dispatch(bugResolved(1));
