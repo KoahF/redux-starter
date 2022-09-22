8e7ab8f5e64cbfdb6e78a51639b13581c0f1bc0f
KoahF <khoakg2013@gmail.com>
2022-09-22 14:59:52 +0700

    9 - Subscribing to the Store

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import store from './store';
 
+const unsubscribe = store.subscribe(() => {
+	console.log("Store changed!", store.getState());
+})
+
+store.dispatch({
+	type: 'bugAdded',
+	payload: {
+		description: 'Bug 1',
+	},
+});
+
+store.dispatch({
+	type: "bugRemoved",
+	payload: {
+		id: 1,
+	}
+})
