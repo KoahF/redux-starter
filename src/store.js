304031e5df9b0a1119d711b03f3e31d8eb060f72
KoahF <khoakg2013@gmail.com>
2022-09-23 13:33:42 +0700

    Config devtools trace, source map

--- src/store.js
+++ src/store.js
@@ -1,9 +1,6 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
-
-const store = createStore(
-	reducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+import { devToolsEnhancer } from 'redux-devtools-extension';
+const store = createStore(reducer, devToolsEnhancer({ trace: true }));
 
 export default store;
