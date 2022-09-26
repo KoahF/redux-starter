f8428c4c04f75c050c739536643d7e141471c2e6
KoahF <khoakg2013@gmail.com>
2022-09-26 15:56:26 +0700

    Add members slice

--- src/store/entities.js
+++ src/store/entities.js
@@ -1,8 +1,10 @@
-import { combineReducers } from "@reduxjs/toolkit";
+import { combineReducers } from '@reduxjs/toolkit';
 import bugsReducer from './bugs';
 import projectsReducer from './projects';
+import membersReducer from './members';
 
 export default combineReducers({
 	bugs: bugsReducer,
-	projects: projectsReducer
-})
+	projects: projectsReducer,
+	members: membersReducer,
+});
---
+++ src/store/members.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+let lastId = 0;
+const slice = createSlice({
+	name: members,
+	initialState: [],
+	reducers: {
+		memberAdded: (members, action) => {
+			members.push({
+				id: ++lastId,
+				name: action.payload.name,
+				bugIds: [],
+			});
+		},
+	},
+});
+
+export const { memberAdded } = slice.actions;
+export default slice.reducer;
