812ce18d7c7b233ac126cd31f60578b0664b7278
KoahF <khoakg2013@gmail.com>
2022-09-28 14:00:30 +0700

    toast excercise

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from './middleware/logger';
 import reducer from './reducer';
+import toast from './middleware/toast';
 
 export default function () {
 	return configureStore({
@@ -8,6 +9,7 @@ export default function () {
 		middleware: (getDefaultMiddleware) => [
 			...getDefaultMiddleware(),
 			logger({ destination: 'console' }),
+			toast,
 		],
 	});
 }
