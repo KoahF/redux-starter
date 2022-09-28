a6939bf42646c93243cfc1774887ad6ab02d0c3a
KoahF <khoakg2013@gmail.com>
2022-09-28 15:31:32 +0700

    Refactor common default api actions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import configureStore from './store/configureStore';
 import { bugAdded, bugResolved, getUnresolvedBugs } from './store/bugs';
 import { projectAdded } from './store/projects';
-
+import * as actions from './store/api';
 const store = configureStore();
 
-store.dispatch({
-	type: 'apiCallBegan',
-	payload: {
+store.dispatch(
+	actions.apiCallBegan({
 		url: '/bugs',
 		onSuccess: 'bugsReceived',
-		onError: 'apiRequestFailed',
-	},
-});
+	})
+);
--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
+import * as actions from '../api';
+const api =
+	({ dispatch }) =>
+	(next) =>
+	async (action) => {
+		if (action.type !== actions.apiCallBegan.type) return next(action);
 
-const api = ({ dispatch }) => (next) => async (action) => {
-		if (action.type !== 'apiCallBegan') return next(action);
-		
 		next(action);
-		
-		const { url, method, data, onSuccess, onError } = action.payload;
 
+		const { url, method, data, onSuccess, onError } = action.payload;
 		try {
 			const response = await axios.request({
 				baseURL: 'http://localhost:9001/api',
@@ -14,10 +16,16 @@ const api = ({ dispatch }) => (next) => async (action) => {
 				method,
 				data,
 			});
-			dispatch({ type: onSuccess, payload: response.data });
+			// General
+			dispatch(actions.apiCallSuccess(response.data));
+			// Specific
+			if (onSuccess) dispatch({ type: onSuccess, payload: response.data });
 		} catch (e) {
-			dispatch({ type: onError, payload: e });
+			// General
+			dispatch(actions.apiCallFailed(e));
+			// Specific
+			if (onError) dispatch({ type: onError, payload: e });
 		}
 	};
 
-export default api
+export default api;
