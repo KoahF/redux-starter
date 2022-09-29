dc2117c22e3cad23ab0f33fda6f6e09da40d4453
KoahF <khoakg2013@gmail.com>
2022-09-29 09:42:55 +0700

    Getting data from the store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import configureStore from './store/configureStore';
-import { bugAdded, bugResolved, getUnresolvedBugs } from './store/bugs';
-import { projectAdded } from './store/projects';
-import * as actions from './store/api';
+import { loadBugs } from './store/bugs';
+
 const store = configureStore();
 
-store.dispatch(
-	actions.apiCallBegan({
-		url: '/bugs',
-		onSuccess: 'bugsReceived',
-	})
-);
+store.dispatch(loadBugs());
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,36 +1,48 @@
 import { createSelector, createSlice } from '@reduxjs/toolkit';
+import { apiCallBegan } from './api';
 
 let lastId = 0;
 const slice = createSlice({
 	name: 'bugs',
-	initialState: [],
+	initialState: {
+		list: [],
+		loading: false,
+		lastFetch: null,
+	},
 	reducers: {
+		bugsReceived: (bugs, action) => {
+			bugs.list = action.payload;
+		},
 		bugAdded: (bugs, action) => {
-			bugs.push({
+			bugs.list.push({
 				id: ++lastId,
 				description: action.payload.description,
 				resolved: false,
 			});
 		},
 		bugRemoved: (bugs, action) => {
-			bugs.filter((item) => item.id !== action.payload.id);
+			bugs.list.filter((item) => item.id !== action.payload.id);
 		},
 		bugResolved: (bugs, action) => {
 			const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-			bugs[index].resolved = true;
+			bugs.list[index].resolved = true;
 		},
 	},
 });
 
-// Selectors
-// export function getUnresolvedBugs(state) {
-// 	return state.entities.bugs.filter((bug) => !bug.resolved);
-// }
+export const { bugAdded, bugResolved, bugsReceived } = slice.actions;
+export default slice.reducer;
 
+// Action creators
+const API_ENDPOINT = '/bugs';
+export const loadBugs = () =>
+	apiCallBegan({
+		url: API_ENDPOINT,
+		onSuccess: bugsReceived.type,
+	});
+
+// Selectors
 export const getUnresolvedBugs = createSelector(
 	(state) => state.entities.bugs,
 	(bugs) => bugs.filter((bug) => !bug.resolved)
 );
-
-export const { bugAdded, bugResolved } = slice.actions;
-export default slice.reducer;
